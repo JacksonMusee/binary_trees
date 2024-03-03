6a6fb98a5865c012d1bb5fb4e20757de69083f9a
JacksonMusee <jacksonmutisya94@gmail.com>
2024-03-03 09:14:25 +0000

    Push

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+*binary_tree_insert_right - Insert a right child to a parent node
+*
+*@parent: pointer to the node to insert the right-child in
+*@value: the value to store in the new node
+*
+*Return: pointer to the new node
+*/
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->right = parent->right;
+
+	if (new_node->right != NULL)
+		new_node->right->parent = new_node;
+
+	parent->right = new_node;
+
+	return (new_node);
+}
